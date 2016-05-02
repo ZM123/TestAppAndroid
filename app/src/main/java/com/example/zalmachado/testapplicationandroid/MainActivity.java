9b4186cef2e18ed2f93579dd15115ca76a3caa7d
Zal Machado <zalmachado@Zals-MacBook-Pro.local>
2016-05-02 16:14:59 -0700

    add crash on action button press

--- app/src/main/java/com/example/zalmachado/testapplicationandroid/MainActivity.java
+++ app/src/main/java/com/example/zalmachado/testapplicationandroid/MainActivity.java
@@ -18,12 +18,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                throw new RuntimeException("Simple crash");
             }
         });
     }
